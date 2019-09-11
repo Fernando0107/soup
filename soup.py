eb9c4fbd7805f99762095da60671656b02996ef9
Fernando González <rmfernando@outlook.com>
2019-09-11 15:01:46 -0600

    Item of nav menu test and href of UFMail button were created.

--- soup.py
+++ soup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import requests,sys,csv,json
+import re
 
 # This will get the number of function the user will send as an input
 program = sys.argv
@@ -27,10 +28,15 @@ def Portal():
     address = soup.find("a", {"href": "#myModal"})       #This is the Complete Address of UFM 
     phone = soup.find("a", {"href": "tel:+50223387700"})       #This is the phone 
     mail = soup.find("a", {"href": "mailto:inf@ufm.edu"})       #This is the info mail
+    item = soup.find("table", {"id": "menu-table"})
+    for link in soup.findAll('a', attrs={'href': re.compile("^https://accounts.google.com")}): #for loop to gte the href of UFMail button
+        ufmbtn = link.get('href')
     #print('\nThis is the title with HTML objects: \n', soup.title, '\n')
     print('Title: \n', soup.title.string, '\n')         #This is the Title 
     print('Address: \n',address.text, '\n')
     print('Phone and info email: \n', phone.text, '\n',mail.text, '\n')
+    #print('Item of nav menu:',item.text, '\n')
+    print('Href of "UFMail" button: \n',ufmbtn, '\n')
 
 print('\n<Fernando González>\n')
 
